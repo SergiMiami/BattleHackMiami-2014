a2cb4e6f4a29231b24ca7212b82276e1c352b852
peterpena <peter.a.pena@gmail.com>
2014-02-23 08:34:23 -0800

    Javascript

--- site/js/battlehack.js
+++ site/js/battlehack.js
@@ -1,5 +1,11 @@
 $(function() {
         
-        //Insert code here
+var app = document.createElement('applet');
+app.id= 'Java';
+app.archive= './../bin/HackForKidsApplet.jar';
+app.code= './../bin/StartClass.class';
+app.width = '1000';
+app.height = '1000';
+document.getElementsByTagName('body')[0].appendChild(app);
         
         });
